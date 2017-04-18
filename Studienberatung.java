d4fa78f7acbdf2021461fb8b0873db2f3692fbf0
Timbo <tim.mandelbaum@haw-hamburg.de>
2017-04-18 21:34:04 +0200

    hast-du-abitur

--- Studienberatung.java
+++ Studienberatung.java
@@ -7,7 +7,14 @@ public class Studienberatung{
 		System.out.println("Hast du Abitur?");
 		String eingabe = scanner.next();
 		if(eingabe.equals("ja")){
-			 
+			System.out.println("Bist du wissbegierig?");
+			eingabe = scanner.next();
+			if(eingabe.equals("ja")){
+				System.out.println("Auf jeden Fall studieren!");
+			}
+			else{
+				System.out.println("Nicht studieren.");
+			}
 		}
 		else{
 			 
